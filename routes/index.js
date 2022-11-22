8d0ed86228b9ee34e24fa1a201a198611c06ea85
amargh2 <margherioa@gmail.com>
2022-11-21 20:45:57 -0600

    adding validators for sign up form and made sign up form view, some basic styling for the form added

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,6 @@
 var express = require('express');
 var router = express.Router();
-
+const {body, validationResult} = require('express-validator');
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'The Discourse' });
@@ -9,5 +9,35 @@ router.get('/', function(req, res, next) {
 router.get('/login', function(req, res, next) {
   res.render('login', {title: 'Please log in'})
 })
+
+router.get('/sign-up-form', function(req, res, next) {
+  res.render('sign-up-form', {title: 'Sign Up'})
+})
+
+router.post('/sign-up-form', async function(req, res, next) {
+  //check if the form data is valid
+  body('first_name').isAlpha();
+  body('last_name').isAlpha();
+  body('username').isLength({min: 4})
+  body('password').isStrongPassword()
+  body('password').equals(req.body.password, req.body.confirmpassword)
+  //check for errors and send error response if present
+  
+  //valid, so save new user to the database
+  try {
+    mongoose.connect(process.env.MONGO_URI)
+    const user = new User({
+      first_name: req.body.first_name,
+      last_name: req.body.last_name,
+      birthday: req.body.last_name,
+      messages:[]
+    })
+    await user.save()
+    res.redirect('/')
+  } catch (error) {
+    res.render('error', {error:error})
+  }
+})
+
 
 module.exports = router;
