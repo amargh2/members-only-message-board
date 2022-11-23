3d2478b07525640d7c56517678b20ffa40ddd1d2
amargh2 <margherioa@gmail.com>
2022-11-23 15:21:50 -0600

    writing tests for validation and making sure the response contains proper errors

--- routes/index.js
+++ routes/index.js
@@ -16,32 +16,36 @@ router.get('/sign-up-form', function(req, res, next) {
   res.render('sign-up-form', {title: 'Sign Up'})
 })
 
-router.post('/sign-up-form', async function(req, res, next) {
+router.post('/sign-up-form', 
+body('password').isStrongPassword(), 
+async function(req, res, next) {
   //check if the form data is valid
   /*body('first_name').isAlpha();
   body('last_name').isAlpha();
   body('username').isLength({min: 4})
   body('password').isStrongPassword()
   body('password').equals(req.body.password, req.body.confirmpassword)*/
-  //check for errors and send error response if present
-  //valid, so save new user to the database
-  try {
-    mongoose.connect(process.env.MONGO_URI)
-    const user = new User({
-      first_name: req.body.first_name,
-      username: req.body.username,
-      last_name: req.body.last_name,
-      password: req.body.password,
-      birthday:req.body.birthday,
-      messages:[]
-    })
+  const errors = validationResult(req);
+  console.log(errors)
+  !errors.isEmpty() ? res.render('sign-up-form', {errors:errors.array()}) : 
+    async () => {
+    try {
+      mongoose.connect(process.env.MONGO_URI)
+      const user = new User({
+        first_name: req.body.first_name,
+        username: req.body.username,
+        last_name: req.body.last_name,
+        password: req.body.password,
+        birthday:req.body.birthday,
+        messages:[]
+      })
     console.log(user)
     user.save()
     res.redirect('/')
   } catch (error) {
     res.render('error', {error:error})
   }
-})
+}})
 
 
 module.exports = router;
--- tests/tests.js
+++ tests/tests.js
@@ -4,6 +4,9 @@ const app = require('../app')
 const mongoose = require('mongoose')
 const expect = require('chai').expect
 const request = require('supertest');
+const chai = require('chai');
+const { response } = require('../app');
+chai.use(require('chai-dom'))
 
 describe('Message Board', function() {
   it('should understand basic math', function(done) {
@@ -11,7 +14,7 @@ describe('Message Board', function() {
   }
 )})
 
-describe('User registration', function(done) {
+describe('User registration', function() {
   //connect to db before making requests
   before(function () {
     mongoose.connect(process.env.MONGO_URI)
@@ -28,13 +31,92 @@ describe('User registration', function(done) {
         password: 'DummyPassword2!',
         confirmpassword: 'DummyPassword2!',
         birthday:'6-16-1988'
-      }).end(function (err, res) {
+      }).then(function (err, res) {
         if (err) throw err
       })
     const user = async () => await User.find({username:'ideogesis'}).lean()
     expect(user()).to.include('ideogesis', done())
   })
+
+  describe('validation', function(){
+    it('rejects bad passwords', async function(done) {
+      //sending post req to sign up form with bad password
+      const responseObject = request(app)
+        .post('/sign-up-form')
+        .set('Content-Type', 'application/json')
+        .set('Accept', 'application/json')
+        .send({
+          first_name:'Anthony',
+          password:'ezpw',
+          username:'loler',
+          last_name: 'Margherio',
+          birthday: '6-16-88'
+        }).then(response => {
+          return response
+        })
+        expect(responseObject).to.have.text('Invalid Password', done())
+      })
+
+      it('rejects bad passwords', async function(done) {
+        //sending post req to sign up form with bad password
+        const responseObject = request(app)
+          .post('/sign-up-form')
+          .set('Content-Type', 'application/json')
+          .set('Accept', 'application/json')
+          .send({
+            first_name:'Anthony',
+            password:'ezpw',
+            username:'loler',
+            last_name: 'Margherio',
+            birthday: '6-16-88'
+          }).then(response => {
+            return response
+          })
+          expect(responseObject).to.have.text('Invalid Password', done())
+        })
+
+
+        it('rejects bad passwords', async function(done) {
+          //sending post req to sign up form with bad password
+          const responseObject = request(app)
+            .post('/sign-up-form')
+            .set('Content-Type', 'application/json')
+            .set('Accept', 'application/json')
+            .send({
+              first_name:'Anthony',
+              password:'ezpw',
+              username:'loler',
+              last_name: 'Margherio',
+              birthday: '6-16-88'
+            }).then(response => {
+              return response
+            })
+            expect(responseObject).to.have.text('Invalid Password', done())
+          })
+
+
+          it('rejects bad passwords', async function(done) {
+            //sending post req to sign up form with bad password
+            const responseObject = request(app)
+              .post('/sign-up-form')
+              .set('Content-Type', 'application/json')
+              .set('Accept', 'application/json')
+              .send({
+                first_name:'Anthony',
+                password:'ezpw',
+                username:'loler',
+                last_name: 'Margherio',
+                birthday: '6-16-88'
+              }).then(response => {
+                return response
+              })
+              expect(responseObject).to.have.text('Invalid Password', done())
+            })
+  })
+
+  //delete records, execute done callback when complete
   after(async function (done) {
     await User.deleteMany({}, done())
   })
+
 })
