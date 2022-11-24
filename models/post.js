2a7fd85ae2446d8cc4093a2c9e5dc31145aad04a
amargh2 <margherioa@gmail.com>
2022-11-24 17:24:31 -0600

    created post model

---
+++ models/post.js
@@ -0,0 +1,12 @@
+const mongoose = require('mongoose')
+const Schema = mongoose.Schema
+
+const Post = new Schema ({
+  author: {type:Schema.Types.ObjectId, ref:'user'},
+  username: String,
+  password: String,
+  date:String,
+  message: String,
+})
+
+module.exports = mongoose.model('Post', Post)
