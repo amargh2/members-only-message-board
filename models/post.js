abfa1aa90b8415b4914bef77d450eeb208ca843b
amargh2 <margherioa@gmail.com>
2022-11-25 11:41:43 -0600

    added objectid ref to post model and corrected a typo

--- models/post.js
+++ models/post.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose')
 const Schema = mongoose.Schema
 
 const Post = new Schema ({
-  author: {type:Schema.Types.ObjectId, ref:'user'},
+  author: {type:Schema.Types.ObjectId, ref:'User'},
   date:String,
   message: String,
 })
