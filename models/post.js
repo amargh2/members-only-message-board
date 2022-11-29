0d06ff03dd6c83b43686efdeaf0bf6f8bbfb5cde
amargh2 <margherioa@gmail.com>
2022-11-29 10:13:22 -0600

    changed post date field to date object from string

--- models/post.js
+++ models/post.js
@@ -3,8 +3,10 @@ const Schema = mongoose.Schema
 
 const Post = new Schema ({
   author: {type:Schema.Types.ObjectId, ref:'User'},
-  date:String,
+  date:Date,
   message: String,
+  replies: [{type:Schema.Types.ObjectId, ref:'Reply', ref:'User'}],
+  reply: {type: Boolean, default:false}
 })
 
 module.exports = mongoose.model('Post', Post)
